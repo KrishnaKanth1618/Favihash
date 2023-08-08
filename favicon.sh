a49ec16d3d689eb3c23bdd7f5af9ffd6235c078e
KrishnaKanth1618 <43302757+KrishnaKanth1618@users.noreply.github.com>
2023-08-08 06:07:05 -0400

    Update favicon.sh

--- favicon.sh
+++ favicon.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 read -p "Enter Shodan API Key: " API_KEY
 read -p "Enter favicon URL: " fav
-##hash
+##Generate hash
 hash=$(curl -s -L -k $fav | python3 -c 'import mmh3,sys,codecs;print(mmh3.hash(codecs.encode(sys.stdin.buffer.read(),"base64")))')
 ##Hash Found
 echo Generated Hash:$hash
