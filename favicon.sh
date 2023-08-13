933d96ca2d3c6d344777484bd676617cede3a861
KrishnaKanth1618 <43302757+KrishnaKanth1618@users.noreply.github.com>
2023-08-14 01:24:56 +0530

    Update favicon.sh

--- favicon.sh
+++ favicon.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-read -p "Enter Shodan API Key: " API_KEY
-read -p "Enter favicon URL: " fav
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+read -p "${GREEN}Enter Shodan API Key: " API_KEY
+read -p "${GREEN}Enter favicon URL: " fav
 ##Generate hash
 hash=$(curl -s -L -k $fav | python3 -c 'import mmh3,sys,codecs;print(mmh3.hash(codecs.encode(sys.stdin.buffer.read(),"base64")))')
 ##Hash Found
@@ -9,4 +11,4 @@ echo Generated Hash:$hash
 shodan_output=$(curl -s "https://api.shodan.io/shodan/host/search?key=$API_KEY&query=http.favicon.hash:$hash")
 ## Print Hosts obtained from search
 host=$(echo "$shodan_output" | jq -r '.matches[].http.host')
-echo "Hosts: $host"
+echo "${RED}Hosts: $host"
